430965745b71b3af5f369ec46f3c1292482162ea
vicky8451 <vicky8451@gmail.com>
2022-12-11 16:05:47 +0800

    Update adoptData.vue

--- src/components/memBasicData/adoptData.vue
+++ src/components/memBasicData/adoptData.vue
@@ -2,68 +2,208 @@
     <div class="adoptData">
         <p id="title">領養資料</p>
         <div class="workspace">
-                <ul class="dropdown-menu position-static d-grid gap-1 mx-0 shadow w-220px">
+            <ul class="dropdown-menu position-static d-grid gap-1 mx-0 shadow w-220px">
+                <form class="row g-3">
                     <li>
-                        <p class="dropdown-item rounded-2 active">1210</p>
-                    </li>
-                    <li>
-                        <div></div>
-                    </li>
-                    <li>
-                        <p class="dropdown-item rounded-2 active">1210</p>
-                    </li>
-                    <li>
-                        <div class="time">
-    
+                        體型偏好：
+                        <div class="form-check form-check-inline">
+                            <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1"
+                                value="option1">
+                            <label class="form-check-label" for="inlineRadio1">沒有特別偏好</label>
+                        </div>
+                        <div class="form-check form-check-inline">
+                            <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2"
+                                value="option2">
+                            <label class="form-check-label" for="inlineRadio2">大型犬</label>
+                        </div>
+                        <div class="form-check form-check-inline">
+                            <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio3"
+                                value="option3">
+                            <label class="form-check-label" for="inlineRadio3">中型犬</label>
+                        </div>
+                        <div class="form-check form-check-inline">
+                            <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio4"
+                                value="option4">
+                            <label class="form-check-label" for="inlineRadio4">小型犬</label>
                         </div>
                     </li>
-                </ul>
-            </div>
+                    <li>
+                        年齡偏好：
+                        <div class="form-check form-check-inline">
+                            <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1"
+                                value="option1">
+                            <label class="form-check-label" for="inlineRadio1">沒有特別偏好</label>
+                        </div>
+                        <div class="form-check form-check-inline">
+                            <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2"
+                                value="option2">
+                            <label class="form-check-label" for="inlineRadio2">2歲以下</label>
+                        </div>
+                        <div class="form-check form-check-inline">
+                            <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio3"
+                                value="option3">
+                            <label class="form-check-label" for="inlineRadio3">2～5歲</label>
+                        </div>
+                        <div class="form-check form-check-inline">
+                            <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio4"
+                                value="option4">
+                            <label class="form-check-label" for="inlineRadio4">5歲以上</label>
+                        </div>
+                    </li>
+                    <li>
+                        毛色偏好：
+                        <div class="form-check form-check-inline">
+                            <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1"
+                                value="option1">
+                            <label class="form-check-label" for="inlineRadio1">沒有特別偏好</label>
+                        </div>
+                        <div class="form-check form-check-inline">
+                            <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2"
+                                value="option2">
+                            <label class="form-check-label" for="inlineRadio2">白色</label>
+                        </div>
+                        <div class="form-check form-check-inline">
+                            <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio3"
+                                value="option3">
+                            <label class="form-check-label" for="inlineRadio3">黑色</label>
+                        </div>
+                        <div class="form-check form-check-inline">
+                            <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio4"
+                                value="option4">
+                            <label class="form-check-label" for="inlineRadio4">黃色</label>
+                        </div>
+                        <div class="form-check form-check-inline">
+                            <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio5"
+                                value="option5">
+                            <label class="form-check-label" for="inlineRadio4">花花色</label>
+                        </div>
+                    </li>
+                    <li>
+                        毛髮偏好：
+                        <div class="form-check form-check-inline">
+                            <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1"
+                                value="option1">
+                            <label class="form-check-label" for="inlineRadio1">沒有特別偏好</label>
+                        </div>
+                        <div class="form-check form-check-inline">
+                            <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2"
+                                value="option2">
+                            <label class="form-check-label" for="inlineRadio2">長毛</label>
+                        </div>
+                        <div class="form-check form-check-inline">
+                            <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio3"
+                                value="option3">
+                            <label class="form-check-label" for="inlineRadio3">短毛</label>
+                        </div>
+                    </li>
+                    <li>
+                        性別偏好：
+                        <div class="form-check form-check-inline">
+                            <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1"
+                                value="option1">
+                            <label class="form-check-label" for="inlineRadio1">沒有特別偏好</label>
+                        </div>
+                        <div class="form-check form-check-inline">
+                            <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2"
+                                value="option2">
+                            <label class="form-check-label" for="inlineRadio2">男生</label>
+                        </div>
+                        <div class="form-check form-check-inline">
+                            <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio3"
+                                value="option3">
+                            <label class="form-check-label" for="inlineRadio3">女生</label>
+                        </div>
+                    </li>
+                    <li>
+                        品種偏好：
+                        <div class="form-check form-check-inline">
+                            <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1"
+                                value="option1">
+                            <label class="form-check-label" for="inlineRadio1">沒有特別偏好</label>
+                        </div>
+                        <div class="form-check form-check-inline">
+                            <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2"
+                                value="option2">
+                            <label class="form-check-label" for="inlineRadio2">只限品種狗</label>
+                        </div>
+                    </li>
+
+                    <div class="col-12">
+                        <label for="inputAddress" class="form-label">簡述家庭環境：</label>
+                        <input type="text" class="form-control" id="inputAddress" placeholder="(如空間大小，有無院子等)"
+                            style="height: 80px">
+                    </div>
+
+                    <li>
+                        <button type="button" class="cancel">取消更改</button>
+                        <button type="button" class="confirm">確認更改</button>
+                    </li>
+                </form>
+
+            </ul>
+        </div>
     </div>
-    </template>
+</template>
     
-    <script></script>
+<script></script>
     
-    <style scoped>
-    .time {
-        background-color: #EEEEEE;
-        border-radius: 27px;
-    }
-    .workspace ul {
-      display: block;
-      height: 100%;
-      background-color: #F9ECBE;
-      border-radius: 40px;
-      padding: 2rem!important;
-      --bs-dropdown-border-color: none;
-      --bs-dropdown-border-radius: none;
-    }
-    .workspace li p{
-      font-size: 30px;
-      font-weight: bold;
-      color: black !important;
-      background-color: #F9ECBE!important;
-      display: flex;
-      align-items: center!important;
-      height: 100%;
-    }
-    p {
-        font-weight: bold;
-        color: black;
-    }
-    #title {
-        font-size: 50px;
-        padding-left: 40%;
-        display: flex;
-        justify-content: start;
-    }
-    .workspace {
-        position: absolute;
-        transform: translate(-50%, -50%);
-        top:60%;
-        left: 50%;
-        width: 55%;
-        height: 70%;
-        font-size: 30px;
-    }
-    </style>
+<style scoped>
+.workspace ul {
+    font-size: 25px;
+    display: block;
+    height: 100%;
+    background-color: #F9ECBE;
+    border-radius: 40px;
+    padding: 2rem !important;
+    --bs-dropdown-border-color: none;
+    --bs-dropdown-border-radius: none;
+}
+
+.workspace li p {
+    font-size: 30px;
+    color: black !important;
+    background-color: #F9ECBE !important;
+    display: flex;
+    align-items: center !important;
+    height: 100%;
+}
+
+p {
+    color: black;
+}
+
+#title {
+    font-size: 50px;
+    font-weight: bold;
+    padding-left: 42%;
+    display: flex;
+    justify-content: start;
+}
+
+.workspace {
+    position: absolute;
+    transform: translate(-50%, -50%);
+    top: 60%;
+    left: 50%;
+    width: 65%;
+    height: 70%;
+    font-size: 30px;
+}
+
+.cancel {
+    float: left;
+    color: white;
+    background: #9A9DA2;
+    border-radius: 20px;
+    font-size: 40px;
+}
+
+.confirm {
+    font-family: 'Iansui094-Regular';
+    float: right;
+    color: white;
+    background: #114ABA;
+    border-radius: 20px;
+    font-size: 40px;
+}
+</style>
