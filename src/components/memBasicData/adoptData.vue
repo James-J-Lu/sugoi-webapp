38f80432062df5573b410187ef0804b8c3ea43a2
vicky8451 <vicky8451@gmail.com>
2022-12-11 18:25:23 +0800

    Update adoptData.vue

--- src/components/memBasicData/adoptData.vue
+++ src/components/memBasicData/adoptData.vue
@@ -30,22 +30,22 @@
                     <li>
                         年齡偏好：
                         <div class="form-check form-check-inline">
-                            <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1"
+                            <input class="form-check-input" type="radio" name="inlineRadioOptions1" id="inlineRadio1"
                                 value="option1">
                             <label class="form-check-label" for="inlineRadio1">沒有特別偏好</label>
                         </div>
                         <div class="form-check form-check-inline">
-                            <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2"
+                            <input class="form-check-input" type="radio" name="inlineRadioOptions1" id="inlineRadio2"
                                 value="option2">
                             <label class="form-check-label" for="inlineRadio2">2歲以下</label>
                         </div>
                         <div class="form-check form-check-inline">
-                            <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio3"
+                            <input class="form-check-input" type="radio" name="inlineRadioOptions1" id="inlineRadio3"
                                 value="option3">
                             <label class="form-check-label" for="inlineRadio3">2～5歲</label>
                         </div>
                         <div class="form-check form-check-inline">
-                            <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio4"
+                            <input class="form-check-input" type="radio" name="inlineRadioOptions1" id="inlineRadio4"
                                 value="option4">
                             <label class="form-check-label" for="inlineRadio4">5歲以上</label>
                         </div>
@@ -53,27 +53,27 @@
                     <li>
                         毛色偏好：
                         <div class="form-check form-check-inline">
-                            <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1"
+                            <input class="form-check-input" type="radio" name="inlineRadioOptions2" id="inlineRadio1"
                                 value="option1">
                             <label class="form-check-label" for="inlineRadio1">沒有特別偏好</label>
                         </div>
                         <div class="form-check form-check-inline">
-                            <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2"
+                            <input class="form-check-input" type="radio" name="inlineRadioOptions2" id="inlineRadio2"
                                 value="option2">
                             <label class="form-check-label" for="inlineRadio2">白色</label>
                         </div>
                         <div class="form-check form-check-inline">
-                            <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio3"
+                            <input class="form-check-input" type="radio" name="inlineRadioOptions2" id="inlineRadio3"
                                 value="option3">
                             <label class="form-check-label" for="inlineRadio3">黑色</label>
                         </div>
                         <div class="form-check form-check-inline">
-                            <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio4"
+                            <input class="form-check-input" type="radio" name="inlineRadioOptions2" id="inlineRadio4"
                                 value="option4">
                             <label class="form-check-label" for="inlineRadio4">黃色</label>
                         </div>
                         <div class="form-check form-check-inline">
-                            <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio5"
+                            <input class="form-check-input" type="radio" name="inlineRadioOptions2" id="inlineRadio5"
                                 value="option5">
                             <label class="form-check-label" for="inlineRadio4">花花色</label>
                         </div>
@@ -81,17 +81,17 @@
                     <li>
                         毛髮偏好：
                         <div class="form-check form-check-inline">
-                            <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1"
+                            <input class="form-check-input" type="radio" name="inlineRadioOptions3" id="inlineRadio1"
                                 value="option1">
                             <label class="form-check-label" for="inlineRadio1">沒有特別偏好</label>
                         </div>
                         <div class="form-check form-check-inline">
-                            <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2"
+                            <input class="form-check-input" type="radio" name="inlineRadioOptions3" id="inlineRadio2"
                                 value="option2">
                             <label class="form-check-label" for="inlineRadio2">長毛</label>
                         </div>
                         <div class="form-check form-check-inline">
-                            <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio3"
+                            <input class="form-check-input" type="radio" name="inlineRadioOptions3" id="inlineRadio3"
                                 value="option3">
                             <label class="form-check-label" for="inlineRadio3">短毛</label>
                         </div>
@@ -99,17 +99,17 @@
                     <li>
                         性別偏好：
                         <div class="form-check form-check-inline">
-                            <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1"
+                            <input class="form-check-input" type="radio" name="inlineRadioOptions4" id="inlineRadio1"
                                 value="option1">
                             <label class="form-check-label" for="inlineRadio1">沒有特別偏好</label>
                         </div>
                         <div class="form-check form-check-inline">
-                            <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2"
+                            <input class="form-check-input" type="radio" name="inlineRadioOptions4" id="inlineRadio2"
                                 value="option2">
                             <label class="form-check-label" for="inlineRadio2">男生</label>
                         </div>
                         <div class="form-check form-check-inline">
-                            <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio3"
+                            <input class="form-check-input" type="radio" name="inlineRadioOptions4" id="inlineRadio3"
                                 value="option3">
                             <label class="form-check-label" for="inlineRadio3">女生</label>
                         </div>
@@ -117,12 +117,12 @@
                     <li>
                         品種偏好：
                         <div class="form-check form-check-inline">
-                            <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1"
+                            <input class="form-check-input" type="radio" name="inlineRadioOptions5" id="inlineRadio1"
                                 value="option1">
                             <label class="form-check-label" for="inlineRadio1">沒有特別偏好</label>
                         </div>
                         <div class="form-check form-check-inline">
-                            <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2"
+                            <input class="form-check-input" type="radio" name="inlineRadioOptions5" id="inlineRadio2"
                                 value="option2">
                             <label class="form-check-label" for="inlineRadio2">只限品種狗</label>
                         </div>
@@ -131,7 +131,7 @@
                     <div class="col-12">
                         <label for="inputAddress" class="form-label">簡述家庭環境：</label>
                         <input type="text" class="form-control" id="inputAddress" placeholder="(如空間大小，有無院子等)"
-                            style="height: 80px">
+                            style="height: 110px">
                     </div>
 
                     <li>
@@ -188,6 +188,12 @@ p {
     width: 65%;
     height: 70%;
     font-size: 30px;
+}
+.form-check{
+    font-size: 30px;
+}
+.form-control{
+    font-size: 35px;
 }
 
 .cancel {
