3407f1e19d82e8c6979029e297e43360dc612228
mengju06 <100680536+mengju06@users.noreply.github.com>
2022-12-15 01:40:31 +0800

    12-15-01:40

--- src/components/managerComponent/MroomAmount.vue
+++ src/components/managerComponent/MroomAmount.vue
@@ -6,20 +6,23 @@
                 <div class="row">
                     <div class="col">
                         <label for="cc-name" class="form-label">大房間：{{bigRoom}}間</label>
-                        <input type="number" class="form-control inputDisplay" v-model="bigRoom" min="0" max="10">
+                        <input type="number" class="form-control inputDisplay" v-model="bigRoom2" min="0">
                     </div>
 
                     <div class="col">
                         <label for="cc-number" class="form-label">中房間：{{middleRoom}}間</label>
-                        <input type="number" class="form-control inputDisplay" v-model="middleRoom" min="0" max="10">
+                        <input type="number" class="form-control inputDisplay" v-model="middleRoom2" min="0">
                     </div>
 
                     <div class="col">
                         <label for="cc-number" class="form-label">小房間：{{smallRoom}}間</label>
-                        <input type="number" class="form-control inputDisplay" v-model="smallRoom" min="0" max="10">
+                        <input type="number" class="form-control inputDisplay" v-model="smallRoom2" min="0">
                     </div>
                 </div>
             </div>
+        </div>
+        <div>
+            <input type="button" value="確認修改" @click="modify(bigRoom2, middleRoom2, smallRoom2)">
         </div>
     </div>
     
@@ -36,10 +39,22 @@
             bigRoom: 10,
             middleRoom: 10,
             smallRoom: 10,
+            bigRoom2: null,
+            middleRoom2: null,
+            smallRoom2: null
         }
     },
     methods: {
-      
+        modify(big2, middle2, small2) {
+            this.bigRoom = big2
+            this.middleRoom = middle2
+            this.smallRoom = small2
+        }
+    },
+    mounted() {
+        this.bigRoom2 = this.bigRoom,
+        this.middleRoom2 = this.middleRoom,
+        this.smallRoom2 = this.smallRoom
     }
 }
 </script>
