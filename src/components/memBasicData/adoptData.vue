526075963e27e1dea1762fe5e92800b065570a3d
vicky8451 <vicky8451@gmail.com>
2022-12-14 21:59:49 +0800

    Update adoptData.vue

--- src/components/memBasicData/adoptData.vue
+++ src/components/memBasicData/adoptData.vue
@@ -3,78 +3,170 @@
         <p id="title">領養資料</p>
         <div class="workspace">
             <ul class="dropdown-menu position-static d-grid gap-1 mx-0 shadow w-220px">
-                <form class="row g-3">
-                    <li>
-                        <label><span>體型偏好：</span></label>
-                        <input type="radio" id="s0" value="nosize" v-model="size" name="inlineRadioOptions0">
-                        <label for="one">沒有特別偏好</label>
-                        <input type="radio" id="s1" value="big" v-model="size" name="inlineRadioOptions0">
-                        <label for="two">大型犬</label>
-                        <input type="radio" id="s2" value="medium" v-model="size" name="inlineRadioOptions0">
-                        <label for="three">中型犬</label>
-                        <input type="radio" id="s3" value="small" v-model="size" name="inlineRadioOptions0">
-                        <label for="four">小型犬</label>
-                    </li>
-                    <li>
-                        <label><span>年齡偏好：</span></label>
-                        <input type="radio" id="a0" value="noage" v-model="age" name="inlineRadioOptions1">
-                        <label for="one">沒有特別偏好</label>
-                        <input type="radio" id="a1" value="below2" v-model="age" name="inlineRadioOptions1">
-                        <label for="two">2歲以下</label>
-                        <input type="radio" id="a2" value="2to5" v-model="age" name="inlineRadioOptions1">
-                        <label for="three">2～5歲</label>
-                        <input type="radio" id="a3" value="above5" v-model="age" name="inlineRadioOptions1">
-                        <label for="four">5歲以上</label>
-                    </li>
-                    <li>
-                        <label><span>毛色偏好：</span></label>
-                        <input type="radio" id="c0" value="nocolor" v-model="color" name="inlineRadioOptions2">
-                        <label for="one">沒有特別偏好</label>
-                        <input type="radio" id="c1" value="white" v-model="color" name="inlineRadioOptions2">
-                        <label for="two">白色</label>
-                        <input type="radio" id="c2" value="black" v-model="color" name="inlineRadioOptions2">
-                        <label for="three">黑色</label>
-                        <input type="radio" id="c3" value="yellow" v-model="color" name="inlineRadioOptions2">
-                        <label for="four">黃色</label>
-                        <input type="radio" id="c4" value="multi" v-model="color" name="inlineRadioOptions2">
-                        <label for="five">花花色</label>
-                    </li>
-                    <li>
-                        <label><span>毛髮偏好：</span></label>
-                        <input type="radio" id="h0" value="nohair" v-model="hair" name="inlineRadioOptions3">
-                        <label for="one">沒有特別偏好</label>
-                        <input type="radio" id="h1" value="long" v-model="hair" name="inlineRadioOptions3">
-                        <label for="two">長毛</label>
-                        <input type="radio" id="h2" value="short" v-model="hair" name="inlineRadioOptions3">
-                        <label for="three">短毛</label>
-                    </li>
-                    <li>
-                        <label><span>性別偏好：</span></label>
-                        <input type="radio" id="s0" value="nosexual" v-model="sexual" name="inlineRadioOptions4">
-                        <label for="one">沒有特別偏好</label>
-                        <input type="radio" id="s1" value="boy" v-model="sexual" name="inlineRadioOptions4">
-                        <label for="two">男生</label>
-                        <input type="radio" id="s2" value="girl" v-model="sexual" name="inlineRadioOptions4">
-                        <label for="three">女生</label>
-                    </li>
-                    <li>
-                        <label><span>品種偏好：</span></label>
-                        <input type="radio" id="b0" value="nobreed" v-model="breed" name="inlineRadioOptions5">
-                        <label for="one">沒有特別偏好</label>
-                        <input type="radio" id="b1" value="pure" v-model="breed" name="inlineRadioOptions5">
-                        <label for="two">只限品種狗</label>
-                    </li>
 
-                    <div class="col-12">
-                        <label for="background" class="form-label">簡述家庭環境：</label>
-                        <input type="text" class="form-control" id="background" v-model="background"
-                            placeholder="(如空間大小，有無院子等)" style="height: 110px">
+                <form class="row g-3">
+                    <div class="first" v-if="visibility">
+                        <li>
+                            <label><span>體型偏好：</span></label>
+                            <input type="radio" id="s0" value="nosize" v-model="size" name="inlineRadioOptions0"
+                                disabled>
+                            <label for="one">沒有特別偏好</label>
+                            <input type="radio" id="s1" value="big" v-model="size" name="inlineRadioOptions0" disabled>
+                            <label for="two">大型犬</label>
+                            <input type="radio" id="s2" value="medium" v-model="size" name="inlineRadioOptions0"
+                                disabled>
+                            <label for="three">中型犬</label>
+                            <input type="radio" id="s3" value="small" v-model="size" name="inlineRadioOptions0"
+                                disabled>
+                            <label for="four">小型犬</label>
+                        </li>
+                        <li>
+                            <label><span>年齡偏好：</span></label>
+                            <input type="radio" id="a0" value="noage" v-model="age" name="inlineRadioOptions1" disabled>
+                            <label for="one">沒有特別偏好</label>
+                            <input type="radio" id="a1" value="below2" v-model="age" name="inlineRadioOptions1"
+                                disabled>
+                            <label for="two">2歲以下</label>
+                            <input type="radio" id="a2" value="2to5" v-model="age" name="inlineRadioOptions1" disabled>
+                            <label for="three">2～5歲</label>
+                            <input type="radio" id="a3" value="above5" v-model="age" name="inlineRadioOptions1"
+                                disabled>
+                            <label for="four">5歲以上</label>
+                        </li>
+                        <li>
+                            <label><span>毛色偏好：</span></label>
+                            <input type="radio" id="c0" value="nocolor" v-model="color" name="inlineRadioOptions2"
+                                disabled>
+                            <label for="one">沒有特別偏好</label>
+                            <input type="radio" id="c1" value="white" v-model="color" name="inlineRadioOptions2"
+                                disabled>
+                            <label for="two">白色</label>
+                            <input type="radio" id="c2" value="black" v-model="color" name="inlineRadioOptions2"
+                                disabled>
+                            <label for="three">黑色</label>
+                            <input type="radio" id="c3" value="yellow" v-model="color" name="inlineRadioOptions2"
+                                disabled>
+                            <label for="four">黃色</label>
+                            <input type="radio" id="c4" value="multi" v-model="color" name="inlineRadioOptions2"
+                                disabled>
+                            <label for="five">花花色</label>
+                        </li>
+                        <li>
+                            <label><span>毛髮偏好：</span></label>
+                            <input type="radio" id="h0" value="nohair" v-model="hair" name="inlineRadioOptions3"
+                                disabled>
+                            <label for="one">沒有特別偏好</label>
+                            <input type="radio" id="h1" value="long" v-model="hair" name="inlineRadioOptions3" disabled>
+                            <label for="two">長毛</label>
+                            <input type="radio" id="h2" value="short" v-model="hair" name="inlineRadioOptions3"
+                                disabled>
+                            <label for="three">短毛</label>
+                        </li>
+                        <li>
+                            <label><span>性別偏好：</span></label>
+                            <input type="radio" id="s0" value="nosexual" v-model="sexual" name="inlineRadioOptions4"
+                                disabled>
+                            <label for="one">沒有特別偏好</label>
+                            <input type="radio" id="s1" value="boy" v-model="sexual" name="inlineRadioOptions4"
+                                disabled>
+                            <label for="two">男生</label>
+                            <input type="radio" id="s2" value="girl" v-model="sexual" name="inlineRadioOptions4"
+                                disabled>
+                            <label for="three">女生</label>
+                        </li>
+                        <li>
+                            <label><span>品種偏好：</span></label>
+                            <input type="radio" id="b0" value="nobreed" v-model="breed" name="inlineRadioOptions5"
+                                disabled>
+                            <label for="one">沒有特別偏好</label>
+                            <input type="radio" id="b1" value="pure" v-model="breed" name="inlineRadioOptions5"
+                                disabled>
+                            <label for="two">只限品種狗</label>
+                        </li>
+
+                        <div class="col-12">
+                            <label for="background" class="form-label">簡述家庭環境：</label>
+                            <p>{{ background }}</p>
+                        </div>
+                        <li>
+                            <button type="button" class="confirm" v-on:click="edit">編輯</button>
+                        </li>
+                    </div>
+                    <div class="second" v-if="!visibility">
+                        <li>
+                            <label><span>體型偏好：</span></label>
+                            <input type="radio" id="s0" value="nosize" v-model="size" name="inlineRadioOptions0">
+                            <label for="one">沒有特別偏好</label>
+                            <input type="radio" id="s1" value="big" v-model="size" name="inlineRadioOptions0">
+                            <label for="two">大型犬</label>
+                            <input type="radio" id="s2" value="medium" v-model="size" name="inlineRadioOptions0">
+                            <label for="three">中型犬</label>
+                            <input type="radio" id="s3" value="small" v-model="size" name="inlineRadioOptions0">
+                            <label for="four">小型犬</label>
+                        </li>
+                        <li>
+                            <label><span>年齡偏好：</span></label>
+                            <input type="radio" id="a0" value="noage" v-model="age" name="inlineRadioOptions1">
+                            <label for="one">沒有特別偏好</label>
+                            <input type="radio" id="a1" value="below2" v-model="age" name="inlineRadioOptions1">
+                            <label for="two">2歲以下</label>
+                            <input type="radio" id="a2" value="2to5" v-model="age" name="inlineRadioOptions1">
+                            <label for="three">2～5歲</label>
+                            <input type="radio" id="a3" value="above5" v-model="age" name="inlineRadioOptions1">
+                            <label for="four">5歲以上</label>
+                        </li>
+                        <li>
+                            <label><span>毛色偏好：</span></label>
+                            <input type="radio" id="c0" value="nocolor" v-model="color" name="inlineRadioOptions2">
+                            <label for="one">沒有特別偏好</label>
+                            <input type="radio" id="c1" value="white" v-model="color" name="inlineRadioOptions2">
+                            <label for="two">白色</label>
+                            <input type="radio" id="c2" value="black" v-model="color" name="inlineRadioOptions2">
+                            <label for="three">黑色</label>
+                            <input type="radio" id="c3" value="yellow" v-model="color" name="inlineRadioOptions2">
+                            <label for="four">黃色</label>
+                            <input type="radio" id="c4" value="multi" v-model="color" name="inlineRadioOptions2">
+                            <label for="five">花花色</label>
+                        </li>
+                        <li>
+                            <label><span>毛髮偏好：</span></label>
+                            <input type="radio" id="h0" value="nohair" v-model="hair" name="inlineRadioOptions3">
+                            <label for="one">沒有特別偏好</label>
+                            <input type="radio" id="h1" value="long" v-model="hair" name="inlineRadioOptions3">
+                            <label for="two">長毛</label>
+                            <input type="radio" id="h2" value="short" v-model="hair" name="inlineRadioOptions3">
+                            <label for="three">短毛</label>
+                        </li>
+                        <li>
+                            <label><span>性別偏好：</span></label>
+                            <input type="radio" id="s0" value="nosexual" v-model="sexual" name="inlineRadioOptions4">
+                            <label for="one">沒有特別偏好</label>
+                            <input type="radio" id="s1" value="boy" v-model="sexual" name="inlineRadioOptions4">
+                            <label for="two">男生</label>
+                            <input type="radio" id="s2" value="girl" v-model="sexual" name="inlineRadioOptions4">
+                            <label for="three">女生</label>
+                        </li>
+                        <li>
+                            <label><span>品種偏好：</span></label>
+                            <input type="radio" id="b0" value="nobreed" v-model="breed" name="inlineRadioOptions5">
+                            <label for="one">沒有特別偏好</label>
+                            <input type="radio" id="b1" value="pure" v-model="breed" name="inlineRadioOptions5">
+                            <label for="two">只限品種狗</label>
+                        </li>
+
+                        <div class="col-12">
+                            <label for="background" class="form-label">簡述家庭環境：</label>
+                            <input type="text" class="form-control" id="background" v-model="background"
+                                placeholder="(如空間大小，有無院子等)" style="height: 110px">
+                        </div>
+                        <li>
+                            <button type="button" class="cancel" v-on:click="cancel">取消更改</button>
+                            <button type="button" class="confirm">確認更改</button>
+                        </li>
+
                     </div>
 
-                    <li>
-                        <button type="button" class="cancel">取消更改</button>
-                        <button type="button" class="confirm">確認更改</button>
-                    </li>
+
                 </form>
 
             </ul>
@@ -84,7 +176,7 @@
     
 <script>
 export default {
-    name: 'MmemberData',
+    name: 'adoptData',
     components: {
     },
     data() {
@@ -96,8 +188,18 @@ export default {
             sexual: "boy",
             breed: "pure",
             background: "我家有水池",
+            visibility: true
+        }
+    },
+    methods: {
+        cancel() {
+            this.visibility = true
+        },
+        edit() {
+            this.visibility = false
         }
     }
+
 }
 </script>
     
