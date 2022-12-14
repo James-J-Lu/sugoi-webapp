4b3b1b64aa0e79936f3ec46bc2515fd788da4fe8
jhaumei <fghj050067@gmail.com>
2022-12-14 16:02:52 +0800

    use bootstrap

--- src/components/memBasicData/myOrder.vue
+++ src/components/memBasicData/myOrder.vue
@@ -1,6 +1,18 @@
 <template>
     <div class="myOrder">
         <p>我的訂單</p>
+        <div>
+            <ul class="nav nav-tabs">
+                <li class="nav-item">
+                    <a class="nav-link active" href="#">托兒訂單</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">領養訂單</a>
+                </li>
+                
+                
+            </ul>
+        </div>
     </div>
 </template>
 
