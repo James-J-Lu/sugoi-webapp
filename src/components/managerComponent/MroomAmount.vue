b16dac7439dd9b54390d3862b1b0e53811686b4a
mengju06 <100680536+mengju06@users.noreply.github.com>
2022-12-14 16:48:30 +0800

    12-14-16:48

--- src/components/managerComponent/MroomAmount.vue
+++ src/components/managerComponent/MroomAmount.vue
@@ -2,15 +2,47 @@
     <div class="MroomAmount">
         <p class="Rtop">查詢房間數量</p>
         <div class="roomDisplay">
-            <div>大房間</div>
-            <div>中房間</div>
-            <div>小房間</div>
+            <div class="container">
+                <div class="row">
+                    <div class="col">
+                        <label for="cc-name" class="form-label">大房間：{{bigRoom}}間</label>
+                        <input type="number" class="form-control inputDisplay" v-model="bigRoom" :value="bigRoom">
+                    </div>
+
+                    <div class="col">
+                        <label for="cc-number" class="form-label">中房間：{{middleRoom}}間</label>
+                        <input type="number" class="form-control inputDisplay" v-model="middleRoom" :value="middleRoom">
+                    </div>
+
+                    <div class="col">
+                        <label for="cc-number" class="form-label">小房間：{{smallRoom}}間</label>
+                        <input type="number" class="form-control inputDisplay" v-model="smallRoom"  :value="smallRoom">
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
     
 </template>
 
-<script></script>
+<script>
+    export default {
+    name: 'MroomAmount',
+    components: {
+        
+    },
+    data () {
+        return {
+            bigRoom: 10,
+            middleRoom: 10,
+            smallRoom: 10,
+        }
+    },
+    methods: {
+      
+    }
+}
+</script>
 
 <style>
 .Rtop {
@@ -24,5 +56,9 @@
 .roomDisplay {
     font-size: 40px;
     margin-top: 20%;
+}
+.inputDisplay {
+    font-size: 28px;
+    text-align: center;
 }
 </style>
