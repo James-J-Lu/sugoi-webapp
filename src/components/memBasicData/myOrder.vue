a1657142fc850ddc6dbc9e48352efe545f739a00
jhaumei <fghj050067@gmail.com>
2022-12-14 15:41:46 +0800

    update data

--- src/components/memBasicData/myOrder.vue
+++ src/components/memBasicData/myOrder.vue
@@ -10,13 +10,13 @@
         </button>
 
         <div v-if="show" class="workspace">
-            <tabNursery v-if="currentTab == '托兒訂單'"></tabNursery>
-            <tabAdpot v-if="currentTab == '領養訂單'"></tabAdpot>
+            <tabNurseryNoData v-if="currentTab == '托兒訂單'" class="nurseryNoData"></tabNurseryNoData>
+            <tabAdpotNoData v-if="currentTab == '領養訂單'" class="nurseryNoData"></tabAdpotNoData>
 
 
         </div>
-        <div v-if="!show">
-
+        <div v-if="!show" class="workspace">
+            <div v-for="nurseryOrder in nurseryOrders" :key="nurseryOrders.id" :name="nurseryOrders.name" @click="">
         </div>
 
         
@@ -30,15 +30,20 @@ export default{
             show: true,
             currentTab: "托兒訂單",
             tabs:['托兒訂單','領養訂單'],
-            data: [
-                // {id: , name: ,},
-                // {},
-            ]
+            nurseryOrders: [
+                {id: 123, petName: 'cookie',},
+                {id: 124, petName: 'apple'},
+            ],
+            adoptOrders: [
+                {id: 333, petName: 'mandy'},
+                {id: 444, petName: 'teddy'},
+            ],
+            goToOrderDetail: false
         }
     },
     components:{
-        'tabNursery': <div>content in tab1</div>,
-        'tabAdpot': <div>content in tab2</div>,
+        'tabNurseryNoData': <div>目前還沒有任何托兒訂單噢！</div>,
+        'tabAdpotNoData': <div>目前還沒有任何領養訂單噢！</div>,
     }
 }
 
@@ -56,9 +61,26 @@ export default{
     height: 85%;
     font-size: 30px;
     background-color: rgba(249, 236, 190, 0.44);
+    border-radius: 40px;
 }
 
 .tab-button {
     background-color: aquamarine;
+}
+
+.nurseryNoData{
+    position: relative;
+    left: 15%;
+    width: 70%;
+    height: 85%;
+    top: 200px;
+}
+
+.adoptNoData{
+    position: relative;
+    left: 15%;
+    width: 70%;
+    height: 85%;
+    top: 200px;
 }
 </style>
