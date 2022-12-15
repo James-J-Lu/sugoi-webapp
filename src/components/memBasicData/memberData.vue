5ebca766145df355f5f5ed934a16feedca8cfca5
vicky8451 <vicky8451@gmail.com>
2022-12-15 23:14:02 +0800

    Update memberData.vue

--- src/components/memBasicData/memberData.vue
+++ src/components/memBasicData/memberData.vue
@@ -2,6 +2,12 @@
     <div class="memberData">
         <p id="title">會員基本資料</p>
         <div class="workspace">
+            <div v-if="pop && visibility" class="alert alert-warning alert-dismissible fade show" role="alert">
+                <strong>{{ members }}您好：</strong> 會員資料已更新
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"
+                    v-on:click="popoff"></button>
+            </div>
+            <br>
             <form class="row g-3">
                 <ul class="dropdown-menu position-static d-grid gap-1 mx-0 shadow w-220px">
                     <div class="first" v-if="visibility">
@@ -26,34 +32,39 @@
                         <br>
                         <label><span>地址：{{ address }}</span></label>
                         <li>
-                        
                             <button type="button" class="confirm" v-on:click="edit">編輯</button>
                         </li>
                     </div>
                     <div class="second" v-if="!visibility">
-                       
-                            <label><span>姓名：<input style="background-color:#F9ECBE" name="name" v-model="members_t" class="form-control " placeholder="請輸入姓名"></span></label>
-                            <br>
-                            <label><span>  性別：</span></label>
-                            <input type="radio" id="one" value="male" v-model="picked_t">
-                            <label for="one">男性</label>
-                            <input type="radio" id="two" value="female" v-model="picked_t">
-                            <label for="two">女性</label>
-                        
+
+                        <label><span>姓名：<input style="background-color:#F9ECBE" name="name" v-model="members_t"
+                                    class="form-control " placeholder="請輸入姓名"></span></label>
+                        <br>
+                        <label><span> 性別：</span></label>
+                        <input type="radio" id="one" value="male" v-model="picked_t">
+                        <label for="one">男性</label>
+                        <input type="radio" id="two" value="female" v-model="picked_t">
+                        <label for="two">女性</label>
+
                         <br>
                         <label><span>帳號：{{ account }}</span></label>
                         <br>
                         <label><span>生日：</span></label>
-                        <DatePicker style="background-color:#F9ECBE" v-model="date_t" :enable-time-picker="false" :clearable="false" class="input2"></DatePicker>
-                        
-                        <label><span>電話：<input style="background-color:#F9ECBE" type="text" name="telephone" v-model="telephone_t" class="form-control " placeholder="請輸入電話" /></span></label> 
+                        <DatePicker style="background-color:#F9ECBE" v-model="date_t" :enable-time-picker="false"
+                            :clearable="false" class="input2"></DatePicker>
+
+                        <label><span>電話：<input style="background-color:#F9ECBE" type="text" name="telephone"
+                                    v-model="telephone_t" class="form-control " placeholder="請輸入電話" /></span></label>
                         <br>
-                        <label><span>手機：<input style="background-color:#F9ECBE" type="text" name="phonenumber" v-model="phonenumber_t" class="form-control " placeholder="請輸入手機" /></span></label>
+                        <label><span>手機：<input style="background-color:#F9ECBE" type="text" name="phonenumber"
+                                    v-model="phonenumber_t" class="form-control " placeholder="請輸入手機" /></span></label>
                         <br>
-                        <label><span>電子郵箱：<input style="background-color:#F9ECBE" type="email" v-model="email_t" class="form-control " placeholder="請輸入電子郵箱" /></span></label>
+                        <label><span>電子郵箱：<input style="background-color:#F9ECBE" type="email" v-model="email_t"
+                                    class="form-control " placeholder="請輸入電子郵箱" /></span></label>
                         <br>
                         <label><span>地址：</span></label>
-                        <input style="background-color:#F9ECBE"  type="text" class="form-control " placeholder="請輸入地址"  v-model="address_t">
+                        <input style="background-color:#F9ECBE" type="text" class="form-control " placeholder="請輸入地址"
+                            v-model="address_t">
                         <br>
                         <li>
                             <button type="button" class="cancel" v-on:click="cancel">取消更改</button>
@@ -66,7 +77,7 @@
             </form>
         </div>
 
-    
+
 
 
 
@@ -82,6 +93,7 @@
 
 import DatePicker from '@vuepic/vue-datepicker';
 import '@vuepic/vue-datepicker/dist/main.css'
+
 
 export default {
     name: 'MmemberData',
@@ -106,49 +118,57 @@ export default {
             date: "12.01.2022",
             date_t: null,
             visibility: true,
-            pop: false
-        }
-    },
-    methods: {
-        cancel() {
-            this.visibility = true
-            this.pop = false
+            pop: false,
+            now: null,
+            time: null
+        }},
+    
+        methods: {
+            cancel() {
+                this.visibility = true
+                this.pop = false
+            },
+            edit() {
+                this.visibility = false
+                this.members_t = this.members
+                this.picked_t = this.picked
+                this.telephone_t = this.telephone
+                this.phonenumber_t = this.phonenumber
+                this.email_t = this.email
+                this.address_t = this.address
+                this.date_t = this.date
+            },
+            modify(members_t, picked_t, telephone_t, phonenumber_t, email_t, address_t, date_t) {
+                this.members = members_t
+                this.picked = picked_t
+                this.telephone = telephone_t
+                this.phonenumber = phonenumber_t
+                this.email = email_t
+                this.address = address_t
+                this.date = date_t
+                this.pop = true
+                this.visibility = true
+                this.now=new Date()
+            },
+            popoff() {
+                this.pop = false
+            }
+
+
         },
-        edit() {
-            this.visibility = false
-            this.members_t = this.members
-            this.picked_t = this.picked
-            this.telephone_t = this.telephone
-            this.phonenumber_t = this.phonenumber
-            this.email_t = this.email
-            this.address_t = this.address
-            this.date_t = this.date
-        },
-        modify(members_t, picked_t, telephone_t, phonenumber_t, email_t, address_t, date_t) {
-            this.members = members_t
-            this.picked = picked_t
-            this.telephone = telephone_t
-            this.phonenumber = phonenumber_t
-            this.email = email_t
-            this.address = address_t
-            this.date = date_t
-            this.pop = true
-            this.visibility = true
-        }
 
-
-    },
-
-}
+    }
 </script>
 
 <style>
-.first{
+.first {
     font-size: 35px;
 }
-.second{
+
+.second {
     font-size: 35px;
 }
+
 .workspace ul {
     font-size: 25px;
     display: block;
@@ -192,7 +212,9 @@ p {
     font-size: 30px;
 }
 
-
+.alert alert-warning alert-dismissible fade show {
+    border-radius: 20px;
+}
 
 
 .form-check {
@@ -212,7 +234,6 @@ p {
 }
 
 .confirm {
-    font-family: 'Iansui094-Regular';
     float: right;
     color: white;
     background: #114ABA;
