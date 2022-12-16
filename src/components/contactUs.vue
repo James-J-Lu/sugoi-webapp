0a18c76e51f0889e2060d6ed4e0b7543c5c668be
ruxingdong <ruxingdong09@gmail.com>
2022-12-16 23:04:42 +0800

    我們做的事+聯絡我們

--- src/components/contactUs.vue
+++ src/components/contactUs.vue
@@ -1,13 +1,53 @@
 <template>
     <div class="contactUs">
         <p>聯絡我們</p>
+        <div class="contactinfo">
+            <div class="item">
+            <img class="con1" src="../assets/dog.png" alt="">
+            <p>張浩玫110423069</p>
+            </div>
+            <div class="item">
+            <img class="con1" src="../assets/dog.png" alt="">
+            <p>李孟儒111423005</p>
+            </div>
+            <div class="item">
+            <img class="con1" src="../assets/dog.png" alt="">
+            <p>林鈺倫111423024</p>
+            </div>
+            <div class="item">
+            <img class="con1" src="../assets/dog.png" alt="">
+            <p>盧俊吉111423056</p>
+            </div>
+            <div class="item">
+            <img class="con1" src="../assets/dog.png" alt="">
+            <p>黃永璿111423062</p>
+            </div>
+            <div class="item">
+            <img class="con1" src="../assets/dog.png" alt="">
+            <p>董洳杏111423069</p>
+            </div>
+        </div>
     </div>
+    
 </template>
 
 <script></script>
 
 <style>
 .contactUs {
+    font-size: 60px;
+    font-weight: bold;
+    color: black !important;
+}
+.contactinfo {
+    display: flex;
+    flex-wrap: wrap;
+}
+.item{
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
     font-size: 40px;
+    width: 50%;
+    align-items: center;
 }
 </style>
--- src/components/whatWedo.vue
+++ src/components/whatWedo.vue
@@ -1,13 +1,28 @@
 <template>
     <div class="whatWedo">
-        <p>我們在做的事</p>
+        <div>
+            <img class="img1" src="../assets/whatwedo.jpg" alt="sugoidesu">
+        </div>
+        <div>
+        <p>毛孩需要最全面的照顧~</p>
+        <p>外出、旅行玫人照顧嗎？</p>
+        <p>想領養卻苦無門路？</p>
+        <p>我們幫你！</p>
+        </div>
     </div>
 </template>
 
-<script></script>
+<script>
+</script>
 
 <style>
+.img1 {
+  max-width: 100%;
+  height: auto;
+}
 .whatWedo {
     font-size: 40px;
+    font-weight: bold;
+    color: black !important;
 }
 </style>
