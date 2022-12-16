347d72cfc901e821de275e91639e08c032ebfe86
mengju06 <100680536+mengju06@users.noreply.github.com>
2022-12-17 00:26:45 +0800

    12-17-12:26

--- src/components/managerComponent/MadoptPet.vue
+++ src/components/managerComponent/MadoptPet.vue
@@ -26,6 +26,36 @@
     <div v-if="gocheck">
         <div class="card mb-4">
         <div class="card-body">
+          <div class="container py-5 h-100">
+            <div class="row d-flex justify-content-center align-items-center h-100">
+              <div class="col col-lg-6 mb-4 mb-lg-0">
+                <div class="card mb-3" style="border-radius: .5rem;">
+                  <div class="row g-0">
+                    <div class="col-md-4 gradient-custom text-center text-white"
+                      style="border-top-left-radius: .5rem; border-bottom-left-radius: .5rem;">
+                      <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava1-bg.webp"
+                        alt="Avatar" class="img-fluid my-5" style="width: 80px;" />
+                      <h5>Marie Horwitz</h5>
+                      <p>Web Designer</p>
+                      <i class="far fa-edit mb-5"></i>
+                    </div>
+                    <div class="col-md-8">
+                      <div class="card-body p-4">
+                        <div class="row pt-1">
+                          <div class="col-6 mb-3">
+                            <h4>狗狗編號</h4>
+                            <p class="text-muted">{{adoptPets[selectPet].id}}</p>
+                            <h4>狗狗名字</h4>
+                            <p class="text-muted">{{adoptPets[selectPet].name}}</p>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
           <div class="row">
             <div class="col-sm-3">
               <p class="mb-0">體型</p>
