bde2c24e34f75451066162d096fb44237bb2c021
mengju06 <100680536+mengju06@users.noreply.github.com>
2022-12-17 15:17:32 +0800

    12-17-15:17

--- src/components/managerComponent/MadoptPet.vue
+++ src/components/managerComponent/MadoptPet.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="MadoptPet">
       <p class="Ptop">待領養狗狗
-        <input v-if="gocheck" @click="back(gocheck)" class="backButton" type="button" value="返回">
-        <input v-if="!gocheck" @click="checkNew(gonew)" type="button" value="新增">
+        <input v-if="!gomain" @click="back(gocheck)" class="backButton" type="button" value="返回">
+        <input v-if="gomain" @click="checkNew(gonew)" type="button" value="新增">
       </p>
-      <div class="table-responsive" v-if="!gocheck">
+      <div class="table-responsive" v-if="gomain">
         <table class="table table-striped table-sm tableType">
           <thead class="PtableHead">
             <tr>
@@ -122,158 +122,100 @@
         </div>
       </div>
     </div>
-    <div class="table-responsive" v-if="!gonew">
-      <form class="needs-validation" novalidate="">
+    <div class="table-responsive" v-if="gonew">
+      <div class="container py-5 h-100 Pform">
+        <form class="needs-validation">
           <div class="row g-3">
-            <div class="col-sm-6">
-              <label for="firstName" class="form-label">First name</label>
-              <input type="text" class="form-control" id="firstName" placeholder="" value="" required="">
-              <div class="invalid-feedback">
-                Valid first name is required.
+            <div class="row mb-3">
+              <label class="col-sm-4 col-form-label">狗狗名字：</label>
+              <div class="col-sm-3">
+                <input type="text" class="form-control">
               </div>
             </div>
-
-            <div class="col-sm-6">
-              <label for="lastName" class="form-label">Last name</label>
-              <input type="text" class="form-control" id="lastName" placeholder="" value="" required="">
-              <div class="invalid-feedback">
-                Valid last name is required.
+            <div class="my-1">
+              <label for="username" class="form-label">體型：</label>
+              <div class="form-check form-check-inline">
+                <input class="form-check-input" type="radio">
+                <label class="form-check-label" for="inlineRadio1">大型犬</label>
+              </div>
+              <div class="form-check form-check-inline">
+                <input class="form-check-input" type="radio">
+                <label class="form-check-label" for="inlineRadio2">中型犬</label>
+              </div>
+              <div class="form-check form-check-inline">
+                <input class="form-check-input" type="radio">
+                <label class="form-check-label" for="inlineRadio3">小型犬</label>
               </div>
             </div>
-
-            <div class="col-12">
-              <label for="username" class="form-label">Username</label>
-              <div class="input-group has-validation">
-                <span class="input-group-text">@</span>
-                <input type="text" class="form-control" id="username" placeholder="Username" required="">
-              <div class="invalid-feedback">
-                  Your username is required.
-                </div>
+            <div class="row mb-3">
+              <label class="col-sm-4 col-form-label">年齡：</label>
+              <div class="col-sm-1">
+                <input type="text" class="form-control">
               </div>
             </div>
-
-            <div class="col-12">
-              <label for="email" class="form-label">Email <span class="text-muted">(Optional)</span></label>
-              <input type="email" class="form-control" id="email" placeholder="you@example.com">
-              <div class="invalid-feedback">
-                Please enter a valid email address for shipping updates.
+            <div class="my-1">
+              <label for="username" class="form-label">毛色：</label>
+              <div class="form-check form-check-inline">
+                <input class="form-check-input" type="radio">
+                <label class="form-check-label" for="inlineRadio1">黑色</label>
+              </div>
+              <div class="form-check form-check-inline">
+                <input class="form-check-input" type="radio">
+                <label class="form-check-label" for="inlineRadio2">黃色系</label>
+              </div>
+              <div class="form-check form-check-inline">
+                <input class="form-check-input" type="radio">
+                <label class="form-check-label" for="inlineRadio3">棕色系</label>
+              </div>
+              <div class="form-check form-check-inline">
+                <input class="form-check-input" type="radio">
+                <label class="form-check-label" for="inlineRadio3">白色</label>
+              </div>
+              <div class="form-check form-check-inline">
+                <input class="form-check-input" type="radio">
+                <label class="form-check-label" for="inlineRadio3">花白色</label>
+              </div>
+              <div class="form-check form-check-inline">
+                <input class="form-check-input" type="radio">
+                <label class="form-check-label" for="inlineRadio3">其他</label>
               </div>
             </div>
-
-            <div class="col-12">
-              <label for="address" class="form-label">Address</label>
-              <input type="text" class="form-control" id="address" placeholder="1234 Main St" required="">
-              <div class="invalid-feedback">
-                Please enter your shipping address.
+            <div class="my-1">
+              <label for="username" class="form-label">毛的長度：</label>
+              <div class="form-check form-check-inline">
+                <input class="form-check-input" type="radio">
+                <label class="form-check-label" for="inlineRadio1">長毛</label>
+              </div>
+              <div class="form-check form-check-inline">
+                <input class="form-check-input" type="radio">
+                <label class="form-check-label" for="inlineRadio2">短毛</label>
               </div>
             </div>
-
-            <div class="col-12">
-              <label for="address2" class="form-label">Address 2 <span class="text-muted">(Optional)</span></label>
-              <input type="text" class="form-control" id="address2" placeholder="Apartment or suite">
-            </div>
-
-            <div class="col-md-5">
-              <label for="country" class="form-label">Country</label>
-              <select class="form-select" id="country" required="">
-                <option value="">Choose...</option>
-                <option>United States</option>
-              </select>
-              <div class="invalid-feedback">
-                Please select a valid country.
+            <div class="my-1">
+              <label for="username" class="form-label">性別：</label>
+              <div class="form-check form-check-inline">
+                <input class="form-check-input" type="radio">
+                <label class="form-check-label" for="inlineRadio1">男生</label>
+              </div>
+              <div class="form-check form-check-inline">
+                <input class="form-check-input" type="radio">
+                <label class="form-check-label" for="inlineRadio2">女生</label>
               </div>
             </div>
-
-            <div class="col-md-4">
-              <label for="state" class="form-label">State</label>
-              <select class="form-select" id="state" required="">
-                <option value="">Choose...</option>
-                <option>California</option>
-              </select>
-              <div class="invalid-feedback">
-                Please provide a valid state.
+            <div class="row mb-3">
+              <label class="col-sm-4 col-form-label">品種：</label>
+              <div class="col-sm-3">
+                <input type="text" class="form-control">
               </div>
             </div>
-
-            <div class="col-md-3">
-              <label for="zip" class="form-label">Zip</label>
-              <input type="text" class="form-control" id="zip" placeholder="" required="">
-              <div class="invalid-feedback">
-                Zip code required.
-              </div>
+            <div class="form-group">
+              <label for="info">介紹：</label>
+              <textarea name="info" id="info" cols="30" rows="10" class="form-control"></textarea>
             </div>
+            <button class="w-100 btn btn-primary btn-lg" type="submit">確認</button>
           </div>
-
-          <hr class="my-4">
-
-          <div class="form-check">
-            <input type="checkbox" class="form-check-input" id="same-address">
-            <label class="form-check-label" for="same-address">Shipping address is the same as my billing address</label>
-          </div>
-
-          <div class="form-check">
-            <input type="checkbox" class="form-check-input" id="save-info">
-            <label class="form-check-label" for="save-info">Save this information for next time</label>
-          </div>
-
-          <hr class="my-4">
-
-          <h4 class="mb-3">Payment</h4>
-
-          <div class="my-3">
-            <div class="form-check">
-              <input id="credit" name="paymentMethod" type="radio" class="form-check-input" checked="" required="">
-              <label class="form-check-label" for="credit">Credit card</label>
-            </div>
-            <div class="form-check">
-              <input id="debit" name="paymentMethod" type="radio" class="form-check-input" required="">
-              <label class="form-check-label" for="debit">Debit card</label>
-            </div>
-            <div class="form-check">
-              <input id="paypal" name="paymentMethod" type="radio" class="form-check-input" required="">
-              <label class="form-check-label" for="paypal">PayPal</label>
-            </div>
-          </div>
-
-          <div class="row gy-3">
-            <div class="col-md-6">
-              <label for="cc-name" class="form-label">Name on card</label>
-              <input type="text" class="form-control" id="cc-name" placeholder="" required="">
-              <small class="text-muted">Full name as displayed on card</small>
-              <div class="invalid-feedback">
-                Name on card is required
-              </div>
-            </div>
-
-            <div class="col-md-6">
-              <label for="cc-number" class="form-label">Credit card number</label>
-              <input type="text" class="form-control" id="cc-number" placeholder="" required="">
-              <div class="invalid-feedback">
-                Credit card number is required
-              </div>
-            </div>
-
-            <div class="col-md-3">
-              <label for="cc-expiration" class="form-label">Expiration</label>
-              <input type="text" class="form-control" id="cc-expiration" placeholder="" required="">
-              <div class="invalid-feedback">
-                Expiration date required
-              </div>
-            </div>
-
-            <div class="col-md-3">
-              <label for="cc-cvv" class="form-label">CVV</label>
-              <input type="text" class="form-control" id="cc-cvv" placeholder="" required="">
-              <div class="invalid-feedback">
-                Security code required
-              </div>
-            </div>
-          </div>
-
-          <hr class="my-4">
-
-          <button class="w-100 btn btn-primary btn-lg" type="submit">Continue to checkout</button>
         </form>
+      </div>
       </div>
   </div>
 </template>
@@ -303,23 +245,32 @@
                 {id: 13, name:'Nora', description:'', petSize:'', petAge:'', hairColor:'', hairLong:'', petGender:'', petType:'', intro:''},
                 {id: 14, name:'Olga', description:'', petSize:'', petAge:'', hairColor:'', hairLong:'', petGender:'', petType:'', intro:''}
             ],
+            gomain:true,
             gocheck:false,
             gonew:false,
-            gomodify:false,
+            goback:false,
             selectPet: null,
         }
     },
     methods: {
       test(id) {
-        this.gocheck=!this.gocheck
+        this.gocheck=true
+        this.goback=true
+        this.gomain=false
+        this.gonew=false
         this.selectPet = id
       },
       back() {
-        this.gocheck=!this.gocheck
+        this.gomain=true
+        this.gocheck=false
+        this.gonew=false
+        this.goback=false
       },
       checkNew() {
-        this.gocheck=!this.gocheck
-        this.gonew=!this.gonew
+        this.gomain=false
+        this.gocheck=false
+        this.goback=false
+        this.gonew=true
       },
       checkModify() {        
         this.gomodify=!this.gomodify
@@ -358,5 +309,9 @@ input[type=button] {
   float:right; 
   font-size: 20px;
   margin: 4px 2px;
+}
+.Pform {
+  text-align: left;
+  font-size: 28px;
 }
 </style>
